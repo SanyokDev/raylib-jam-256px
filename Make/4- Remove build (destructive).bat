12a4ba6412f0b9cea4e5c9fdcb6bf1d3956e27c7
Alessandro Nikolaev <alessandronikolaev.business@gmail.com>
2022-12-05 01:05:06 +0000

    Updated Remove build script

--- Make/4- Remove build (destructive).bat
+++ Make/4- Remove build (destructive).bat
@@ -3,8 +3,10 @@ set pname=Birball
 
 if exist %pname%.data (del %pname%.data)
 if exist %pname%.html (del %pname%.html)
+if exist index.html (del index.html)
 if exist %pname%.js (del %pname%.js)
 if exist %pname%.wasm (del %pname%.wasm)
+if exist %pname%.zip (del %pname%.zip)
 
 if exist libraylib.a (del libraylib.a)
 
